09e81442929fa7cc2c156a4fe9b136855da30901
Phil Silla <philsilla20@gmail.com>
2024-02-10 22:17:17 +0800

    Add config for api permits

--- src/main/java/com/phl/business/config/AuthorizationServerConfig.java
+++ src/main/java/com/phl/business/config/AuthorizationServerConfig.java
@@ -2,14 +2,28 @@ package com.phl.business.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.net.http.HttpClient;
 
 @Configuration
 public class AuthorizationServerConfig {
 
     @Bean
-    public PasswordEncoder encoder(){
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf()
+                .disable()
+                .authorizeHttpRequests(request -> {
+            request.requestMatchers("/**").permitAll();
+        });
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
 }
