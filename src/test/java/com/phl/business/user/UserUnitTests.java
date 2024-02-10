db59856107419c00029bc8b16c9d5de7c5baa5f9
Phil Silla <philsilla20@gmail.com>
2024-02-10 14:32:10 +0800

    User module - Added Unit Testingszsszsz

---
+++ src/test/java/com/phl/business/user/UserUnitTests.java
@@ -0,0 +1,82 @@
+package com.phl.business.user;
+
+import com.phl.business.domain.user.*;
+import jakarta.annotation.PostConstruct;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class UserUnitTests {
+
+
+    @Autowired
+    UserService userService;
+
+    @PostConstruct
+    void init() {
+        UserRequestDto userRequestDto = UserRequestDto.builder()
+                                                .username("postconstruct")
+                                                .password("postconstruct")
+                                                .build();
+        userService.createUser(userRequestDto);
+    }
+
+    @Test
+    void createUserTest() {
+        UserRequestDto userRequestDto = UserRequestDto.builder()
+                                                .username("phil")
+                                                .password("root")
+                                                .build();
+        UserResponseDto savedUserRequestDto = userService.createUser(userRequestDto);
+
+        assertEquals(savedUserRequestDto.getUsername(), userRequestDto.getUsername());
+        Assertions.assertNotEquals(userService.getAllUsers().size(), 1);
+    }
+
+    @Test
+    void updateUserTest() {
+        User user = userService.getAllUsers().get(1);
+        UserRequestDto userRequestDto = UserRequestDto.builder()
+                                                .username("updatedUsername")
+                                                .build();
+        UserResponseDto updatedUserResponseDto = userService.updateUser(user.getUuid(), userRequestDto);
+        User updatedUser = userService.getOneUser(updatedUserResponseDto.getUuid());
+
+        assertNotEquals(updatedUserResponseDto.getUsername(), user.getUsername());
+        assertEquals(updatedUserResponseDto.getUsername(),updatedUser.getUsername());
+    }
+
+    @Test
+    void getOneUserTest() {
+        String testUUID = "12345";
+        User user = userService.getAllUsers().get(0);
+
+        assertNotNull(userService.getOneUser(user.getUuid()));
+        assertNull(userService.getOneUser(testUUID));
+    }
+
+    @Test
+    void getAllUsersTest() {
+        assertNotEquals(userService.getAllUsers().size(), 0);
+    }
+
+    @Test
+    void deleteUserTest() {
+        UserRequestDto userRequestDto = UserRequestDto.builder()
+                                                .username("forDelete")
+                                                .password("forDelete")
+                                                .build();
+        UserResponseDto userResponseDto = userService.createUser(userRequestDto);
+        String uuidResponse = userService.deleteUser(userResponseDto.getUuid());
+
+        assertNull(userService.getOneUser(uuidResponse));
+
+    }
+}
