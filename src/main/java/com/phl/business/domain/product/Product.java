9f0a9cf54ec665dd96b7bbd4e6eec93b03252f16
Phil Silla <philsilla20@gmail.com>
2024-02-10 13:48:56 +0800

    Product module - updates

--- src/main/java/com/phl/business/domain/product/Product.java
+++ src/main/java/com/phl/business/domain/product/Product.java
@@ -2,12 +2,20 @@ package com.phl.business.domain.product;
 
 import com.phl.business.domain.store.Store;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "ProductTbl")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Product implements Serializable {
 
     @Id
@@ -24,6 +32,13 @@ public class Product implements Serializable {
     @ManyToOne
     @JoinColumn(name = "store_uuid")
     private Store store;
+
+    public Product updateFrom(ProductDto productDto){
+        this.name = productDto.getName();
+        this.description = productDto.getDescription();
+        this.price = productDto.getPrice();
+        this.quantity = productDto.getQuantity();
+    }
 
 
 }
