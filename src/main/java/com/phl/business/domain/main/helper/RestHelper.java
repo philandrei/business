f11576faa06d68b4839af62becd26dcd8f1c009e
Phil Silla <philsilla20@gmail.com>
2024-02-11 22:15:29 +0800

    Moved access of data from Context to RestHelper

--- src/main/java/com/phl/business/domain/main/helper/RestHelper.java
+++ src/main/java/com/phl/business/domain/main/helper/RestHelper.java
@@ -1,10 +1,13 @@
 package com.phl.business.domain.main.helper;
 
+import com.phl.business.domain.authentication.AuthUserDetails;
 import com.phl.business.domain.main.dto.RestResponse;
+import com.phl.business.domain.user.model.User;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -13,6 +16,17 @@ import java.util.Date;
 public class RestHelper {
     @Autowired
     HttpServletRequest httpServletRequest;
+
+    protected User getUser() {
+        AuthUserDetails authUserDetails = (AuthUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return authUserDetails.getUser();
+
+    }
+
+    protected String getClientId() {
+        User user = getUser();
+        return user.getClient().getUuid();
+    }
 
     public ResponseEntity<RestResponse> buildSuccess(Object obj){
         log.info("[buildSuccess] Building success response");
